6c7f28db0f7ae7eeb8b8ead944acf9a254430795
Owen Evans <owen@bgeek.net>
2015-09-16 21:47:16 +1200

    use base box:

--- deploy.sh
+++ deploy.sh
@@ -6,22 +6,22 @@ TIMESTAMP=$(date +%Y%m%d_%H%M%S)
 
 echo "making timestamped deploy directory";
 
-mkdir -p ${ROOT_CONNECTOR_DIR}/hoist-connector-workflowmax/${TIMESTAMP}
+mkdir -p ${ROOT_CONNECTOR_DIR}/hoist-connector-${CONNECTOR_NAME}/${TIMESTAMP}
 
 echo "copying files to deploy directory";
 
-cp -r . ${ROOT_CONNECTOR_DIR}/hoist-connector-workflowmax/${TIMESTAMP}
+cp -r . ${ROOT_CONNECTOR_DIR}/hoist-connector-${CONNECTOR_NAME}/${TIMESTAMP}
 
 echo "removing current symlink";
 
-rm -f ${ROOT_CONNECTOR_DIR}/hoist-connector-workflowmax/current
+rm -f ${ROOT_CONNECTOR_DIR}/hoist-connector-${CONNECTOR_NAME}/current
 
 echo "relinking current symlink";
 
-ln -s ${ROOT_CONNECTOR_DIR}/hoist-connector-workflowmax/${TIMESTAMP} ${ROOT_CONNECTOR_DIR}/hoist-connector-workflowmax/current
+ln -s ${ROOT_CONNECTOR_DIR}/hoist-connector-${CONNECTOR_NAME}/${TIMESTAMP} ${ROOT_CONNECTOR_DIR}/hoist-connector-${CONNECTOR_NAME}/current
 
 echo "removing old deploy directories";
 
-(ls -t ${ROOT_CONNECTOR_DIR}/hoist-connector-workflowmax/|head -n 5;ls ${ROOT_CONNECTOR_DIR}/hoist-connector-workflowmax/)|sort|uniq -u|xargs -I '{}' rm -r ${ROOT_CONNECTOR_DIR}/hoist-connector-workflowmax/'{}'
+(ls -t ${ROOT_CONNECTOR_DIR}/hoist-connector-${CONNECTOR_NAME}/|head -n 5;ls ${ROOT_CONNECTOR_DIR}/hoist-connector-${CONNECTOR_NAME}/)|sort|uniq -u|xargs -I '{}' rm -r ${ROOT_CONNECTOR_DIR}/hoist-connector-${CONNECTOR_NAME}/'{}'
 
 echo "done!";
